1425cf1752ede430016e518d43e629e71d379d61
MisteryPoints <misterypoint2@gmail.com>
2025-08-26 21:52:34 -0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+     <h1 className="">Lets build an API proctivity APP</h1>
+     <Button>Click me</Button>
+    </div>
+  );
+}
