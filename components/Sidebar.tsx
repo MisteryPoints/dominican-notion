dd8b95047ea4f7c315a0f1b71e9858a1c98588f9
Victor Tejada (ClaroDom) <Victor_Tejada@claro.com.do>
2025-11-12 17:03:00 -0400

    Adding Drawer for modern slide-out panels for the navigation menu.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from 'react';
+import NewDocumentButton from './NewDocumentButton';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 
 function Sidebar() {
   return (
-    <div>
-      Sidebar
+    <div className='p-2 md:p-5 bg-gray-200 relative'>
+        <Drawer direction='left'>
+            <DrawerTrigger>Abrir</DrawerTrigger>
+            <DrawerContent>
+                <DrawerHeader>
+                    <DrawerTitle>¿Estás seguro?</DrawerTitle>
+                    <DrawerDescription>
+                        Esta acción, no podrá deshacerse. Esto eliminarJ
+                    </DrawerDescription>
+                </DrawerHeader>
+                <DrawerFooter>
+                    <DrawerClose>Close</DrawerClose>
+                </DrawerFooter>
+            </DrawerContent>
+        </Drawer>
+        <div className="hidden md:inline">
+            <NewDocumentButton />
+        </div> 
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
